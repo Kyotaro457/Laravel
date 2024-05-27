5e376291e13aa7690cecbc691f5d8c2d96f2f525
Kyotaro457 <a20.hhkp@g.chuo-u.ac.jp>
2024-05-27 22:24:12 +0900

    DBへの保存システム完成

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,6 @@ Route::get('/posts',[PostController::class,'index']);
 
 Route::get('/posts/create',[PostController::class,'create']);
 
+Route::post('/posts',[PostController::class,'store']);
+
 Route::get('/posts/{post}',[PostController::class,'show']);
