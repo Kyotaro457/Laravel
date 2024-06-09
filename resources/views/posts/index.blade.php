b4dfaf96557756c5f7745c8f00f056e1bb23d99f
Kyotaro457 <a20.hhkp@g.chuo-u.ac.jp>
2024-06-09 09:00:48 +0900

    delete機能完成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,11 +11,11 @@
     <div class='posts'> 
       @foreach ($posts as $post)
         <div class='post'>
-            <h2 class='title'>
-                <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                </h2>
+            <a href="/posts/{{ $post->id }}">
+            <h2 class='title'>{{ $post->title }}</h2>
+            </a>
             <p class='body'>{{ $post->body }}</p>
-            <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+            <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="POST">
                 @csrf
                 @method('DELETE')
                 <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
