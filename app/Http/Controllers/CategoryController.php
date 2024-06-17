1380c2f1028f694454599cc2a8f2477be8260736
Kyotaro457 <a20.hhkp@g.chuo-u.ac.jp>
2024-06-17 17:28:22 +0900

    category作成

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PostRequest;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index(Category $category)
+        {
+            return view('categories.index')->with(['posts' => $category->getByCategory()]);
+        }
+}
